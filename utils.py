b79b1b34b7aab7aa583aee826a870c8e622b532a
John <dejhswavyguy@gmail.com>
2025-05-07 06:08:06 -0500

    add system prompt

--- utils.py
+++ utils.py
@@ -5,3 +5,6 @@ from dotenv import load_dotenv
 from deepgram import DeepgramClient, PrerecordedOptions, SpeakOptions
 
 load_dotenv()
+
+DP_API_KEY = os.getenv('DEEPGRAM_VOICE_AGENT_KEY')
+OPENAI_API_KEY = os.getenv('OPENAI_LLM_KEY')
